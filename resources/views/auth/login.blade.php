f0aa6f2bc942216b12001f9a7b2331ddeaada8e9
laravel-docker <planet.of.early.summer.rain@gmail.com>
2020-03-21 17:48:20 +0900

    投稿機能追加　ログイン機能追加

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,58 @@
+@extends('app')
+
+@section('title', 'ログイン')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">ログイン</h2>
+
+            @include('error_card_list')
+            
+            <div class="card-text">
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="md-form">
+                  <label for="password">パスワード</label>
+                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <input type="hidden" name="remember" id="remember" value="on">                
+
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
+
+              </form>
+
+              <div class="mt-0">
+                <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
